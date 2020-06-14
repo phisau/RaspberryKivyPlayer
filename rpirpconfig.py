355788296f6298bc8c39e8e9b2d8b0ff45d0f02a
phisau <pony.world@protonmail.com>
2020-06-14 20:58:32 +0200

    Update rpirpconfig.py

--- rpirpconfig.py
+++ rpirpconfig.py
@@ -8,7 +8,10 @@ path_to_folder = "/mnt/nas/musik/"
 fullscreen = False
 
 radio_stations = {
-    "station1": {"name": "SWR 1", "url": "http://mp3-live.swr.de/swr1bw_m.m3u"},
+    "station1": {
+        "name": "SWR 1", 
+        "url": "http://mp3-live.swr.de/swr1bw_m.m3u"
+    },
     "station2": {
         "name": "Deutschland Funk",
         "url": "https://st01.sslstream.dlf.de/dlf/01/high/opus/stream.opus",
